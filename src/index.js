86a2519e8f5241a58a3918c400379256b07082c3
Delaw1 <lawrenceajayi481@gmail.com>
2020-01-05 20:07:28 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-require('./db/mongoose')
+// require('./db/mongoose')
 const indexRouter = require('./router')
 const path = require('path')
 const hbs = require('hbs')
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,86 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.get('/accessories-detail', (req, res) => {
+  res.render('accessories-detail')
+})
+
+router.get('/shopping-cart', (req, res) => {
+  res.render('shopping-cart')
+})
+
+router.get('/checkout', (req, res) => {
+  res.render('checkout')
+})
+
+router.get('/grid1', (req, res) => {
+  res.render('grid1')
+})
+
+router.get('/grid', (req, res) => {
+  res.render('grid')
+})
+
+router.get('/list', (req, res) => {
+  res.render('list')
+})
+
+router.get('/wishlist', (req, res) => {
+  res.render('wishlist')
+})
+
+router.get('/list1', (req, res) => {
+  res.render('list1')
+})
+
+router.get('/car-detail', (req, res) => {
+  res.render('car-detail')
+})
+
+router.get('/blog', (req, res) => {
+  res.render('blog')
+})
+
+router.get('/blog-detail', (req, res) => {
+  res.render('blog-detail')
+})
+
+router.get('/compare', (req, res) => {
+  res.render('compare')
+})
+
+router.get('/about-us', (req, res) => {
+  res.render('about-us')
+})
+
+router.get('/checkout-method', (req, res) => {
+  res.render('checkout-method')
+})
+
+router.get('/checkout-billing-info', (req, res) => {
+  res.render('checkout-billing-info')
+})
+
+router.get('/dashboard', (req, res) => {
+  res.render('dashboard')
+})
+
+router.get('/multiple-addresses', (req, res) => {
+  res.render('multiple-addresses')
+})
+
+router.get('/contact-us', (req, res) => {
+  res.render('contact-us')
+})
+
+router.get('/quickview', (req, res) => {
+  res.render('quickview')
+})
+
+router.get('/newsletter', (req, res) => {
+  res.render('newsletter')
+})
+
 
 router.get("/scrap", async function(req, res, next) {
     const url = 'https://www.copart.com'
